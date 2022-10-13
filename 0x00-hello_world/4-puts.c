9b26769b5313f41d2dd133a466234fcb2c32237e
lefika <lmmelesi@hotmail.com>
2022-10-13 10:43:41 -0700

    fix: added space above puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-	char myStr[] = "Programming is like building a multilingual puzzle";
+int main(void)
+{
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myStr);
-	return 0;
+	return (0);
 }
