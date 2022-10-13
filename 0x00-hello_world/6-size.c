69083f400d351300169c19f89cf81504c398642b
lefika <lmmelesi@hotmail.com>
2022-10-13 13:27:40 -0700

    fix: add int to long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c,\n", sizeof(char));
-	printf("Size of an int: %d,\n", sizeof(int));
-	printf("Size of long int: %ld,\n", sizeof(long));
-	printf("Size of a long long int: %lld\n", sizeof(long long);
-	printf("Size of a float: %f,\n", sizeof(float));
+	printf("Size of a char: %c byte(s),\n", sizeof(char));
+	printf("Size of an int: %d byte(s),\n", sizeof(int));
+	printf("Size of long int: %ld byte(s),\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s),\n", sizeof(long long int);
+	printf("Size of a float: %f byte(s),\n", sizeof(float));
 	return (0);
 }
