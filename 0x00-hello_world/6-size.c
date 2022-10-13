0836f3e215f625f5394c71ff96f775343ee2d710
lefika <lmmelesi@hotmail.com>
2022-10-13 13:52:40 -0700

    add %lu + type cast types to unsigned long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c byte(s),\n", sizeof(char));
-	printf("Size of an int: %d byte(s),\n", sizeof(int));
-	printf("Size of long int: %ld byte(s),\n", sizeof(long int));
-	printf("Size of a long long int: %lld byte(s),\n", sizeof(long long int);
-	printf("Size of a float: %f byte(s),\n", sizeof(float));
+	printf("Size of a char: %lu byte(s),\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s),\n", (unsigned long)sizeof(int));
+	printf("Size of long int: %lu byte(s),\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s),\n", (unsigned long)sizeof(long long int);
+	printf("Size of a float: %lu byte(s),\n", (unsigned long)sizeof(float));
 	return (0);
 }
