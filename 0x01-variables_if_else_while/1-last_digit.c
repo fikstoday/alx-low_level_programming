10938923f46811ad02a630d8f18dc95071699f9b
lefika <lmmelesi@hotmail.com>
2022-10-14 08:34:36 -0700

    fix: correct last if to check if digit not eq 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	if (last < 6 && n != 0)
+	if (last < 6 && last != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
