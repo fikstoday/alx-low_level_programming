2ba2807ad242f505673ba19f39d5b16e4b14f5e8
lefika <lmmelesi@hotmail.com>
2022-10-14 08:03:15 -0700

    check value of n, then display last digit..

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - entry file
+ * @void: parameter ids void
+ *
+ * Description: display last digit >, <, != 0
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 5)
+	{
+		printf("Last digit of %d is and is greater than 5\n", n);
+	}
+	if (n == 0)
+	{
+		printf("Last digit of %d is and is 0\n", n);
+	}
+	if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
