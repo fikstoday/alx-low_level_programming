1d7befb4b61e9f4e59b04c394dc0bdc9b438646e
lefika <lmmelesi@hotmail.com>
2022-10-14 07:37:57 -0700

    display whether n above,equal or less than 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * @void: parameter is void
+ *
+ * Description: printting out whether + or -
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	if (n == 0)
+	{
+		printf("is zero");
+	}
+	if (n < 0)
+	{
+		printf("is negative");
+	}
+	return (0);
+}
