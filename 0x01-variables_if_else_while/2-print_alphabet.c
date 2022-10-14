b89c67a9a782a80619ca4897d13ed17d427db846
lefika <lmmelesi@hotmail.com>
2022-10-14 08:45:55 -0700

    display lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry file
+ * @void: parameter is void
+ *
+ * Description: display alphabet in lowercase
+ * Return: returns 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; a <= "z"; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
