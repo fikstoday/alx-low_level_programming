9509d05bff420289debf09deeea00821fbcd22dc
lefika <lmmelesi@hotmail.com>
2022-10-18 08:05:50 -0700

    display last digit from input

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - start here
+ * @last: parameter is int last
+ *
+ * Description: get the last digit
+ * Return: return last digit
+ */
+int print_last_digit(int last)
+{
+	return (last % 10);
+}
