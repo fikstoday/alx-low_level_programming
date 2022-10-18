568c0cae13db2988093cdd59090e098cc6ba38e1
lefika <lmmelesi@hotmail.com>
2022-10-18 12:42:59 -0700

    remove trailing whitespace

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,36 +10,40 @@
 void jack_bauer(void)
 {
 	int top, next;
-
-	for (top = 0; top < 24; top++)
+		for (top = 0; top < 24; top++)
 	{
 		for (next = 0; next < 60; next++)
 		{
 			if (top < 10 && next < 10)
 			{
 				_putchar('0');
-				_putchar(top);
+				_putchar((top % 10) + '0');
 				_putchar(':');
 				_putchar('0');
-				_putchar(next);
+				_putchar((next % 10) + '0');
+				_putchar('\n');
 				/* printf("0%d:0%d\n", top, next); */
 			}
 			if (top > 10 && next < 10)
 			{
-				_putchar(top + '0');
+				_putchar((top / 10) + '0');
+				_putchar((top % 10) + '0');
 				_putchar(':');
 				_putchar('0');
-				_putchar(next + '0');
+				_putchar((next % 10) + '0');
+				_putchar('\n');
 				/* printf("%d:0%d\n", top, next); */
 			}
 			if (top >= 10 && next >= 10)
 			{
-				_putchar(top + '0');
+				_putchar((top / 10) + '0');
+				_putchar((top % 10) + '0');
 				_putchar(':');
-				_putchar(next + '0');
+				_putchar((next / 10) + '0');
+				_putchar((next % 10) + '0');
+				_putchar('\n');
 				/* printf("%d:%d\n", top, next); */
 			}
-			_putchar('\n');
 		}
 	}
 }
