9d2c1090c27629113dc682a300ba38c21a014c4c
lefika <lmmelesi@hotmail.com>
2022-10-18 07:57:40 -0700

    print number in positive form only

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - start here
+ * @pn: parameter is pn
+ *
+ * Description: return positive result always
+ * Return: positive pn
+ */
+int _abs(int pn)
+{
+	if (pn < 0)
+	{
+		return (-pn);
+	}
+	return (pn);
+}
