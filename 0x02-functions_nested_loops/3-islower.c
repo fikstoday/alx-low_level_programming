2235d83eb0d06a63919dac959654bdcc7cf689b9
lefika <lmmelesi@hotmail.com>
2022-10-18 04:56:20 -0700

    check if input is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * islower - entry for function
+ * @c: parameter is int c
+ *
+ * Description: return umber 1 if c is lowercase
+ * Return: returns 1 or 0
+ */
+int _islower(int c)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if( c == str[i])
+			return (1);
+		else
+			return (0);
+	}
+}
