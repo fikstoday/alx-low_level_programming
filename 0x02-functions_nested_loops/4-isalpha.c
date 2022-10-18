55f5c3bf08030cad0b4eaffa0f17c89577288938
lefika <lmmelesi@hotmail.com>
2022-10-18 06:21:36 -0700

    check if input is letter uppercase,lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - function declaration
+ * @c: parameter is int c
+ *
+ * Description: return 1 if c is a letter
+ * Return: returns 1 or 0
+ */
+int _isalpha(int c)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		if (c == str[i])
+		{
+			return (1);
+		}
+	return (0);
+}
