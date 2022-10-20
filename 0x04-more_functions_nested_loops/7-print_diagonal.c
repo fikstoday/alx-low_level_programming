47bbf1f7de3dc9e63a0876d221d4771837b78c23
lefika <lmmelesi@hotmail.com>
2022-10-20 13:25:08 -0700

    print diagonal line(s)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main"
+/**
+ * print_diagonal - diagonal printing
+ * @n: parameter is int n
+ *
+ * Description: prints diagonal line plus newline
+ * Return: returns nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0 || n < 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			j = 0;
+			_putchar(92);
+			_putchar('\n');
+			while (j < i + 1)
+			{
+				_putchar(32);
+				j++;
+			}
+		}
+	}
+}
