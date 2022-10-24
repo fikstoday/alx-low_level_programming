de0470e519b80649c875ad0c7992ca5bf4b8a4af
lefika <lmmelesi@hotmail.com>
2022-10-24 06:22:41 -0700

    reset input parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value
+ * @n: parameter i s pointer n
+ *
+ * Description: reallocate value to be 98
+ * Return: returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
