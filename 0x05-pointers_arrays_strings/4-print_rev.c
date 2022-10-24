bb80c4bd3955028695a1a8f5d8364d9c2a06999d
lefika <lmmelesi@hotmail.com>
2022-10-24 07:40:05 -0700

    print out string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: parameter of pointer int s
+ *
+ * Description: takes a string thn returns it in reverse
+ * Return: nothing to return
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; i != '\0'; i++)
+	;
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
