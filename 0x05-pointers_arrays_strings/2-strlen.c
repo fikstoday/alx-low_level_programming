1a53b58115abadbfbd5b0997b0e73409a277d74e
lefika <lmmelesi@hotmail.com>
2022-10-24 07:02:48 -0700

    return string length of input

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: parameter of pointer int s
+ *
+ * Description: takes string and returns its length
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	;
+	return (i);
+}
