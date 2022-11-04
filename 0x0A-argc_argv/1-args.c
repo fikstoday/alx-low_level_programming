eb91904a7665fe4509365c0057839acc7ec9f273
lefika <lmmelesi@hotmail.com>
2022-11-04 04:02:05 -0700

    print no of arguments supplied

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -entry
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Description: print count of supplied arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
