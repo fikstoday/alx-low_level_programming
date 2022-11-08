9ca8e27faff79a0cab2b5bebd0d047c9a1237359
lefika <lmmelesi@hotmail.com>
2022-11-08 04:00:01 -0800

    add return for null malloc error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -36,6 +36,18 @@ char *str_concat(char *s1, char *s2)
 	len1 = length(s1);
 	len2 = length(s2);
 	tmp = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (tmp = NULL)
+	{
+		return (tmp);
+	}
+	if (s1 == NULL)
+	{
+		*s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		*s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		tmp[i] = s1[i];
