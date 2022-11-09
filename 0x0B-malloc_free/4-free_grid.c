2f98d56a8a28159d6eacffdde2bdddac273dde61
lefika <lmmelesi@hotmail.com>
2022-11-08 18:34:40 -0800

    release 2 dimensional array memmory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - set grid free
+ * @grid: double pointer
+ * @height: int
+ *
+ * Description: free malloc assigned for grid
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < heightl i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
