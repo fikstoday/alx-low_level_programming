82d5692d3b589855936e7ebac7bbea40c2143c1d
lefika <lmmelesi@hotmail.com>
2022-11-14 17:38:59 -0800

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: void parameter
+ *
+ * Description: main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
