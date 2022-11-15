2f8a8c1d944e195505ffeb961ce4be763933880b
lefika <lmmelesi@hotmail.com>
2022-11-15 06:59:12 -0800

    print out dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - print dog struct
+ * @d: pointer to struct
+ *
+ * Description: print out a struct
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("");
+	}
+	if (d.name == NULL || d.age == NULL || d.owner == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s\n%d\n%s\n", d);
+}
