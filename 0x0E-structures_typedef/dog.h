2ebb7b3520055e196af4f6b95263c899c25851b5
lefika <lmmelesi@hotmail.com>
2022-11-14 18:37:22 -0800

    add comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - dog object
+ * @name: pointer to char
+ * @age: float parameter
+ * @owner: pointer to char
+ *
+ * Description: dog with name, age, owner
+ */
 struct dog
 {
 	char *name;
