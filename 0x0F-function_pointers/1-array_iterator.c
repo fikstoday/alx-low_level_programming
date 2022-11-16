366a1b0f339f8fcf6826e3f07fc1e9b2aed28e55
lefika <lmmelesi@hotmail.com>
2022-11-16 03:31:36 -0800

    print arr of ele via func pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - print every ele
+ * @array: pointer to int
+ * @size: size_t
+ * @action: pointer to func
+ *
+ * Description: action func pointer to print arr of ele
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
