a0838a15801e3462e0d94f50d64ee8712a8e0797
lefika <lmmelesi@hotmail.com>
2022-11-17 01:15:13 -0800

    check if array is null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,9 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (!array || !cmp)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i] == 1))
 		{
 			return (i);
 		}
