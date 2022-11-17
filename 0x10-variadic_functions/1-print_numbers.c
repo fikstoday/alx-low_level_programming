bb97c7e1a7ab4f4ae3ce216072d51896a4fa05a9
lefika <lmmelesi@hotmail.com>
2022-11-17 05:47:48 -0800

    add exit(1)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_numbers - print numbers via variadic
@@ -13,9 +14,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 
 	va_start(ap, n);
+	if (n <= 0)
+	{
+		exit(1);
+	}
 	for (i = 0; i < n; i++)
 	{
-		printf("%c %d", *separator, va_arg(ap, int));
+		if (separator)
+		{
+			printf("%c %d", *separator, va_arg(ap, int));
+		}
+		else
+		{
+			printf(" %d", va_arg(ap, int));
+		}
 	}
 	printf("\n");
 }
