1d34e522632e705532f4884c77ab42fd28a18628
lefika <lmmelesi@hotmail.com>
2022-11-19 07:22:23 -0800

    delete else if

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,13 +21,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", va_arg(ap, int));
 		if (separator != NULL && i != (n-1))
 		{
-			printf("%s%d", separator, va_arg(ap, int));
-		}
-		else if (separator == NULL)
-		{
-			printf("%d", va_arg(ap, int));
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
