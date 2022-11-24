f740f30fe9ac1aa2f0d93ed6325f59a0834104b4
lefika <lmmelesi@hotmail.com>
2022-11-24 03:46:28 -0800

    print no of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * list_len - return no of elements
+ * @h: struct
+ *
+ * Description: print no of elements in a linked list
+ * Return: number
+ */
+size_t list_len(const list_t *h)
+{
+	int length = 0;
+	
+	while(h != NULL)
+	{
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
